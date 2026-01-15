e7306e8d79b7270080bf2632ad813becb647fcd3
LAKSHMI PRABHA V <130069907+Prabha2005@users.noreply.github.com>
2026-01-15 16:47:19 +0530

    Create navItems.js

---
+++ frontend_code/components/navItems.js
@@ -0,0 +1,10 @@
+export const publicLinks = [
+  { name: "Home", path: "/" },
+  { name: "About", path: "/about" },
+];
+
+export const privateLinks = [
+  { name: "Dashboard", path: "/dashboard" },
+  { name: "Chatbot", path: "/chatbot" },
+  { name: "Profile", path: "/profile" },
+];
